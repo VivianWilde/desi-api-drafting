3065ea7640cee2b3e58d2f80f8963ad915d997dc
Vi <goyal.rohan.03@gmail.com>
2023-10-18 16:37:34 -0700

    tests

---
+++ testing.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env ptpython
+from build_spectra import *
+from models import *
+from webapp import *
+
+def test_tile():
+    params = TileParameters(tile=80605, fibers=[10, 234, 2761, 3951])
+    req = ApiRequest(
+        command=Command.DOWNLOAD,
+        request_type=RequestType.TILE,
+        release="fuji",
+        params=params,
+    )
+    return handle(req)
