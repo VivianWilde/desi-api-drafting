ad5ff06481ea5972765e471cee8b398014b04d94
Vi <goyal.rohan.03@gmail.com>
2023-10-18 17:47:21 -0700

    target tests

--- testing.py
+++ testing.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env ptpython
 from build_spectra import *
 from models import *
+
 # from webapp import *
 
+
 def test_tile():
-    params = TileParameters(tile=80605, fibers=[10, 234, 2761, 3951])
+    fibers = [10, 234, 2761, 3951]
+    params = TileParameters(tile=80605, fibers=fibers)
     req = ApiRequest(
         command=Command.DOWNLOAD,
         request_type=RequestType.TILE,
@@ -12,7 +15,27 @@ def test_tile():
         params=params,
     )
     result = handle(req)
-    print(result.fibermap['FIBER', 'TARGETID'])
-    assert sorted(result.fibermap['FIBER'])==[10,234,2761,3951]
+    print(result.fibermap["FIBER", "TARGETID"])
+    assert sorted(result.fibermap["FIBER"]) == sorted(fibers)
+
 
 test_tile()
+
+
+def test_target():
+    targets = [
+        39628473198710603,
+        39632946386177593,
+        39632956452508085,
+        39632971434560784,
+    ]
+    params = TargetParameters(target_ids=targets)
+    req = ApiRequest(
+        command=Command.DOWNLOAD,
+        request_type=RequestType.TARGETS,
+        release="fuji",
+        params=params,
+    )
+    result = handle(req)
+    print(result.fibermap["TARGETID", "FIBER"])
+    assert sorted(result.fibermap["TARGETID"]) == sorted(targets)
